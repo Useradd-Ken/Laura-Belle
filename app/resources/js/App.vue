9566ef4df23605990355271e0c500de1ab328589
KR <111236856+Iamken-rd@users.noreply.github.com>
2026-07-02 15:01:35 +0800

    TechStack Set-Up done, Login UI done.

---
+++ app/resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="min-h-screen bg-white text-white flex items-center justify-center">
+    <div class="text-center space-y-4">
+       <h1 class="text-4xl font-semibold bg-gradient-to-br from-cyan-500 to-blue-500 bg-clip-text text-transparent">
+  Welcome to Laura-Belle
+</h1>
+ <div class="space-y-2 flex flex-col items-center">
+<div class="p-[2px] rounded-md bg-gradient-to-br from-cyan-500 to-blue-500 focus-within:from-blue-500 focus-within:to-cyan-500">
+  <input 
+    type="email" 
+    placeholder="Email@gmail.com" 
+    class="w-full px-2 py-2 rounded-md text-black bg-white outline-none"
+  />
+</div>
+
+<div class="p-[2px] rounded-md bg-gradient-to-br from-cyan-500 to-blue-500 focus-within:from-blue-500 focus-within:to-cyan-500 mt-4">
+  <input 
+    type="password" 
+    placeholder="*********" 
+    class="w-full px-2 py-2 rounded-md text-black bg-white outline-none"
+  />
+</div>
+        <button class="rounded-lg text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-white-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 cursor-pointer">Login</button>
+       </div>
+      
+    </div>
+  </div>
+</template>
